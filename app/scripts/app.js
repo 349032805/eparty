ad4558457c83cf83fe4491865814d83c8dc7251a
349032805@qq.com <349032805@qq.com>
2017-01-23 15:31:56 +0800

    git commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,19 +10,23 @@ angular
         .state("main", {
             url: "/",
             templateUrl: "/views/main.html",
+            cache:'false',
             controller: 'MainCtrl'
         })
         .state("main.index", {
             url: "main/index",
             templateUrl: "/views/index.html",
+            cache:'false',
             controller: 'IndexCtrl'
         })
         .state("main.seller", {
             url: "main/seller",
+            cache:'false',
             templateUrl: "/views/seller.html"
         })
         .state("main.about", {
             url: "main/about",
+            cache:'false',
             templateUrl: "/views/about.html"
         });
        
